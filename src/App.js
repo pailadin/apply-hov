4bff6386e068f6369f7c42c412435acaf5315bd7
Daniel Aranas <danielaranas+git@gmail.com>
2021-06-24 10:41:03 +0800

    Created basic routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+import AnilistPage from 'pages/Anilist';
+import HomePage from 'pages/Home';
+import NotFoundPage from 'pages/NotFound';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path='/anilist' component={AnilistPage} />
+      <Route path='/' component={HomePage} />
+
+      <Route component={NotFoundPage} />
+    </Switch>
   );
 }
-
-export default App;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      TODO
+    </div>
+  )
+}
+
+export default memo(NotFound);
